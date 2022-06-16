287127c9673ad030daaa3e22d6a13622a9af06e9
veclav talica <hectorgreat19@gmail.com>
2022-06-17 04:27:06 +0500

    fixed libc pathing

--- build/nt-gdi.py
+++ build/nt-gdi.py
@@ -2,14 +2,16 @@
 
 # todo: Versions older than Windows 2000 might be runnable too, all the way to Win 95 / NT 3.1, but testing is needed
 
-import subprocess, os
+import subprocess, os, sys
 
 # Uses zig C headers to infer winapi symbols and types
 # A hack for not linking to libc
-zig_path = subprocess.run(["which", "zig"], capture_output=True, check=True)
-zig_dir = os.path.dirname(str(zig_path.stdout, encoding="utf8"))
+zig_path = str(subprocess.run(["which", "zig"], capture_output=True, check=True).stdout, encoding="utf8").strip()
+zig_dir = os.path.dirname(zig_path)
 zig_libc_path = f"{zig_dir}/lib/libc/include/any-windows-any"
-zig_libc_path = "C:\\env\\tools\\zig-windows-x86_64-0.10.0-dev.555+1b6a1e691\\lib\\libc\\include\\any-windows-any"
+if sys.platform == "win32":
+  # Zig doesn't recognize unix paths on windows build it seems?
+  zig_libc_path = str(subprocess.run(["cygpath", "-w", zig_libc_path], capture_output=True, check=True).stdout, encoding="utf8").strip()
 subprocess.run(["zig", "build", f"-Dlibc-path={zig_libc_path}", "-Dtarget=i386-windows.nt4...win10-gnu", "-Drelease-small=true"], check=True)
 
 # Produced executable is not runnable by old NT versions, as PE header is still populated with version 6, which is XP
